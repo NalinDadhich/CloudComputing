7415ef48ee2a50ea1be74a774a9e0ead540e7541
shbhmbhrgv <sbhargava92@gmail.com>
2019-03-06 16:21:54 -0600

    script changes

--- plot_hist.py
+++ plot_hist.py
@@ -2,24 +2,30 @@ import csv
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-	platforms = ["gcp", "aws", "azure"]
+	platforms = ["gcp"]
 	for idx, platform in enumerate(platforms):
-		percentiles = [50, 16, 9, 5, 10, 5, 3, 1, 1]
+		percentiles = [50, 66, 75, 80, 90, 95, 98, 99, 100, 0]
 		csv_name = "logs_"+platform+"/log_500_distribution.csv"
 		csv_file = open(csv_name)
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		time = []
+		time.append(0)
 		for row in csv_reader:
 			if "GET" in row[0]:
 				for col in range(2,11):
 					time.append(float(row[col]))
 		print(time)
 		plt.clf()
-		plt.hist(time,weights=percentiles) # A bar chart
-		plt.xlabel('Time')
-		plt.ylabel('Percentile')
-		plt.ylim(0, 50)
-		for i in range(len(time)):
-			plt.hlines(time[i],0,percentiles[i]) # Here you are drawing the horizontal lines
+		# plt.hist(time,weights=percentiles) # A bar chart
+		plt.xlabel('Time (in ms)')
+		plt.ylabel('Percentile of requests')
+		plt.ylim(0, 100)
+		plt.xlim(0, time[-1])
+		# for i in range(len(time)):
+		# 	plt.hlines(bins=time[i],0,percentiles[i]) # Here you are drawing the horizontal lines
+		data = time
+		for i in range(len(data)):
+			data[i] += 0.5
+		plt.hist(data, bins=time, weights=percentiles, edgecolor='red')
 		name = "Hist_"+platform+".png"
 		plt.savefig(name)
